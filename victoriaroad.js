10a45ab3738c3270b3af1ba0ea4cdc682d450cc8
john layton <johnstewartlayton@gmail.com>
2015-01-28 09:17:00 +1100

    update to express 4

--- victoriaroad.js
+++ victoriaroad.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var app = express();
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
